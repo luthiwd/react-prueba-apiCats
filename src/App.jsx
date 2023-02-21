a885e09be558b9f39b99b8b5ed9fcd5c83a8ba3b
Luthiwd <annattar@gmail.com>
2023-02-21 11:20:14 +0100

    custom hooks,useCatFact, useImageUrl,  service to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,22 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+import { useCatImage } from './hooks/useImageUrl'
+import { useCatFact } from './hooks/useCatFact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&?json=true`
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App () {
-  const [fact, setFact] = useState()
-  const [imageUrl, setImageUrl] = useState()
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
 
-  useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((res) => res.json())
-      .then((data) => {
-        const { fact } = data
-        setFact(fact)
-      })
-  }, [])
-
-  useEffect(() => {
-    if (!fact) return
-    const threeFirstWords = fact.split(' ', 3).join(' ')
-    console.log(threeFirstWords)
-
-    fetch(`https://cataas.com/cat/says/${threeFirstWords}&?json=true`)
-      .then((res) => res.json())
-      .then((response) => {
-        const { url } = response
-        setImageUrl(url)
-      })
-  }, [fact])
+  const handleClick = async () => {
+    refreshFact()
+  }
 
   return (
     <main>
       <h1>APP de Gatitos</h1>
-      <section>
-        {fact && <p>{fact}</p>}
-        {imageUrl && (
-          <img
-            src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
-            alt={`Image extracted using the three first word for  ${fact}`}
-          />
-        )}
-      </section>
+      <button onClick={handleClick}>Get new fact</button>
+      {fact && <p>{fact}</p>}
+      {imageUrl && (<img src={imageUrl} alt={`Image extracted using the three first word for  ${fact}`} />)}
     </main>
   )
 }
